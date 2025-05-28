fedd65fca175efc3065dab81e6426f8aa7c743b9
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-28 16:44:47 +0200

    [UPD] export and import router

--- routers/post.js
+++ routers/post.js
@@ -1,1 +1,17 @@
 const express = require('express');
+
+// creo un router di Express per organizzare le rotte
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+// esporto il router
+module.exports = router;
