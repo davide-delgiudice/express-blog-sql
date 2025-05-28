8f0129d607bd0fec578cac7a2294e522974b66af
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-28 16:54:59 +0200

    [UPD] create controllers folder, postController js file and imported and exported postController

---
+++ controllers/postController.js
@@ -0,0 +1,14 @@
+// metodo index del controller
+const index = (req, res) => {
+    
+}
+
+
+// metodo show del controller
+const show = (req, res) => {
+
+}
+
+module.exports = {
+    index, show
+}
--- routers/post.js
+++ routers/post.js
@@ -3,11 +3,15 @@ const express = require('express');
 // creo un router di Express per organizzare le rotte
 const router = express.Router();
 
+// importo il controller
+const postController = require('../controllers/postController');
 
 
+// rotta index
+router.get('/', postController.index)
 
-
-
+// rotta del singolo post
+router.get('/:id', postController.show)
 
 
 
