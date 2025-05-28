3d2783bd0ee719ddcb8813c90509e817f2061944
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-28 16:25:45 +0200

    [UPD] add express, app and port in app

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+// carico il modulo di express e lo memorizzo
+const express = require('express');
+
+// crea un'istanza dell'applicazione
+const app = express();
+// imposta la porta su cui il server ascolterà
+const port = 3000;
+
+
+// mette il server in ascolto sulla porta specificata
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`)
+})
